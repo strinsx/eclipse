8edfc3cc2dc0e70ed2ee82ce4a8f8033595d53d3
astral.strinsx <157059279+strinsx@users.noreply.github.com>
2026-06-08 17:36:11 +0800

    feat: series interface

---
+++ app/types/tvshows.ts
@@ -0,0 +1,14 @@
+
+
+export interface Series {
+
+    name: string,
+    id: number,
+    original_language: string,
+    media_type: string,
+    overview: string,
+    popularity: number,
+    first_air_date: string,
+    poster_path: string,
+
+}
