094a17f5eb337bbf2ee0fea6ceb232b842953fec
astral.strinsx <157059279+strinsx@users.noreply.github.com>
2026-06-08 17:37:33 +0800

    feat: onair and trendingTV routing

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,10 +1,11 @@
-import { CardPanel } from "../components/Cardpanel";
-import { MoviePanel } from "../components/MoviePanel";
+import { CardPanel } from "../components/movie/Cardpanel";
+import { MoviePanel } from "../components/movie/MoviePanel";
 import { Navbar } from "../components/Navbar";
-import { TrendingPanel } from "../components/TrendingPanel";
+import { TrendingPanel } from "../components/movie/TrendingPanel";
 import {Footer} from '../components/Footer'
-import { TopRatedPanel } from "../components/TopRatedPanel";
-
+import { TopRatedPanel } from "../components/movie/TopRatedPanel";
+import { OnAirTv } from "../components/series/OnAir";
+import { TrendingPanelTV } from "../components/series/TrendingPanelTV";
 export default function Homepage() {
   return (
     <div className="flex flex-col w-[70%] mx-auto min-h-screen">
@@ -13,6 +14,8 @@ export default function Homepage() {
       <MoviePanel />
       <TrendingPanel />
       <TopRatedPanel />
+      <OnAirTv />
+      <TrendingPanelTV />
       <footer>
         <Footer />
       </footer>
