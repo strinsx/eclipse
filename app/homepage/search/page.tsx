6d08d837daa77e7274414a67f4696a4a0dc33c77
astral.strinsx <157059279+strinsx@users.noreply.github.com>
2026-06-17 19:04:05 +0800

    feat: searchpanel components

---
+++ app/homepage/search/page.tsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react";
+import { Navbar } from "@/app/components/Navbar";
+import { Footer } from "@/app/components/Footer";
+import { SearchResults } from "./SearchResults";
+import { Searchpanel } from "@/app/components/movie/Searchpanel";
+
+export default function SearchPage() {
+    return (
+        <div className="flex flex-col w-full md:w-[85%] lg:w-[70%] mx-auto min-h-screen">
+            <Navbar />
+            <Suspense fallback={
+                <div className="flex-1 flex items-center justify-center">
+                    <p className="text-foreground/50">Loading...</p>
+                </div>
+            }>
+
+                <Searchpanel />
+                <SearchResults />
+            </Suspense>
+            <Footer />
+        </div>
+    );
+}
