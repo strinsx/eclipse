b42b565ef4c6f6762da5a990c8ef230debe0e266
astral.strinsx <157059279+strinsx@users.noreply.github.com>
2026-06-20 19:46:18 +0800

    fix: process.env

--- app/lib/supabase/server.ts
+++ app/lib/supabase/server.ts
@@ -5,7 +5,7 @@ export const createClient = async () => {
   const cookieStore = await cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_PUBLIC_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll: () => cookieStore.getAll(),
