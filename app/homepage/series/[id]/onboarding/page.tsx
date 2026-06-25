e4b6e3d6e65462bab24523ab655db314a26b6a5d
astral.strinsx <157059279+strinsx@users.noreply.github.com>
2026-06-25 20:31:26 +0800

    fix: navbar

--- app/homepage/series/[id]/onboarding/page.tsx
+++ app/homepage/series/[id]/onboarding/page.tsx
@@ -1,5 +1,5 @@
 
-import { Navbar } from "@/app/components/GuestNavbar";
+import Navbar from "@/app/components/Navbar";
 import { Footer } from "@/app/components/Footer";
 import { VideoPlayer } from "@/app/components/details/moviedetails/VideoPlayer";
 import { SimilarSeriesGrid } from "@/app/components/series/details/onboarding/Similarseriesgrid";
